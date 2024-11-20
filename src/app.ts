0fc096c2d1b351354071965e9703f1cb9368c669
yashodharpatel <patelyashodhar012@gmail.com>
2024-11-20 17:41:22 +0530

    Final Commit

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ import dotenv from "dotenv";
 
 import connectDB from "./config/dbConnection.ts";
 
+import quizRoutes from "./routes/quiz.route.ts";
+import questionRoutes from "./routes/question.route.ts";
+import responseRoutes from "./routes/response.route.ts";
+
 connectDB();
 
 dotenv.config();
@@ -14,6 +18,10 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use("/api/quizzes", quizRoutes);
+app.use("/api/questions", questionRoutes);
+app.use("/api/responses", responseRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Quiz App Backend is running!");
